819ed0a28c173cb45936dfdefe013af6e0abacd4
Kleyton <kley158@gmail.com>
2020-02-11 21:13:29 -0300

    Retirada do método rad, adição do fatorial auxliar

--- source/taylor.py
+++ source/taylor.py
@@ -3,18 +3,25 @@
 class Taylor:
 
     @classmethod
-    def __init__(cls, x):
+    def __init__(cls, x=0):
         cls.x = x
-
-    @classmethod
-    def radiano(cls):
         cls.rad = (cls.x * 3.1415) / 180
-        return cls.rad
 
     @classmethod
     def formula_inner(cls, num):
-        return (-1)**num * ((cls.r)**2*num)/ # TODO: Implementar fatorial da parte de baixo da função.
+        return (-1)**num * ((cls.rad)**(2*num)/Taylor().fatorial(num=2*num))
+
+    @classmethod
+    def fatorial(cls, num, acc=1):
+        if num > 1:
+            return  Taylor().fatorial(num=num-1, acc=acc*num)
+        else:
+            return acc
+
+    @classmethod
+    def formula_apply(cls):
+        return sum([Taylor(x=cls.x).formula_inner(num=x) for x in range(0, 6)])
 
 
 if __name__ == '__main__':
-    print(Taylor(12).radiano())
+    print(Taylor(5).rad)
