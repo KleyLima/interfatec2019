9587366d46ba5547ca17112ca71f3f4c0a377d49
KleyLima <kley158@gmail.com>
2020-02-12 13:23:45 -0300

    Nakitomon resolvido, passando nos testes do exercicio. O erro era usar classmethod

--- source/nakitomon.py
+++ source/nakitomon.py
@@ -3,69 +3,62 @@
 
 class Nakitomon:
 
-    @classmethod
-    def __init__(cls, strg=0, atk=0, shl=0, agi=0):
-        cls.strg = strg
-        cls.atk = atk
-        cls.shl = shl
-        cls.agi = agi
+    def __init__(self, strg=0, atk=0, shl=0, agi=0):
+        self.strg = strg
+        self.atk = atk
+        self.shl = shl
+        self.agi = agi
 
-    @classmethod
-    def __repr__(cls):
-        return "strg: {}, atk: {}, shl: {}, agi: {}".format(cls.strg, cls.atk, cls.shl, cls.agi)
+    def __repr__(self):
+        return "strg: {}, atk: {}, shl: {}, agi: {}".format(self.strg, self.atk, self.shl, self.agi)
 
 
 class Game:
 
-    @classmethod
-    def __init__(cls):
-        cls.cards = []
-        cls.scr_dan = 0
-        cls.scr_silv = 0
-        cls.draw = 0
-        cls.stats = ["strg", "atk", "shl", "agi"]
+    def __init__(self):
+        self.cards = []
+        self.scr_dan = 0
+        self.scr_silv = 0
+        self.draw = 0
+        self.stats = ["strg", "atk", "shl", "agi"]
 
-    @classmethod
-    def add_card(cls, new: Nakitomon):
+
+    def add_card(self, new: Nakitomon):
         """Adiciona uma nova carta ao baralho"""
-        print(cls.cards)
-        cls.cards.append(new)
+        print(self.cards)
+        self.cards.append(new)
 
-    @classmethod
-    def all_in(cls, cards_dan, cards_silv):
+    def all_in(self, cards_dan, cards_silv):
         """Itera sobre as cartas dos dois jogadores para comparar os atributos e exibe o resultado no fim"""
         print(cards_dan)
         print(cards_silv)
         for dan, silv in zip(cards_dan, cards_silv):
-           cls.one_one(dan, silv)
-        cls.show_res()
+           self.one_one(dan, silv)
+        self.show_res()
 
-    @classmethod
-    def one_one(cls, dan, silv):
+    def one_one(self, dan, silv):
         """Compara os atributos pela ordem prioritária para saber quem venceu"""
-        for stat in cls.stats:
+        for stat in self.stats:
             print(stat)
 
             print(getattr(dan, stat)); print(getattr(silv, stat))
             if getattr(dan, stat) > getattr(silv, stat):
-                cls.scr_dan += 1
+                self.scr_dan += 1
                 return
             elif getattr(silv, stat) > getattr(dan, stat):
-                cls.scr_silv += 1
+                self.scr_silv += 1
                 return
-        cls.draw += 1
+        self.draw += 1
 
-    @classmethod
-    def show_res(cls):
+    def show_res(self):
         """Exibe os resultados do fim da partida"""
-        print("danette venceu:", cls.scr_dan)
-        print("silvio venceu:", cls.scr_silv)
-        print("empates:", cls.draw)
+        print("danette venceu:", self.scr_dan)
+        print("silvio venceu:", self.scr_silv)
+        print("empates:", self.draw)
 
-    @classmethod
-    def inst_card(cls):
+    def inst_card(self):
         strg, atk, shl, agi = [int(x) for x in input().split()]
-        cls.add_card(Nakitomon(strg, atk, shl, agi))
+        self.add_card(Nakitomon(strg, atk, shl, agi))
 
             
 if __name__ == '__main__':
