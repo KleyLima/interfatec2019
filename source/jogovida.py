d568796d8a29bc8f68f645d6f307ef2b2a890bed
Kleyton <kley158@gmail.com>
2020-02-13 00:14:54 -0300

    Fix jogovida

--- source/jogovida.py
+++ source/jogovida.py
@@ -13,24 +13,26 @@ class JogoVida:
             linha = input()
             for y in range(self.col):
                 self.mat[x][y] = int(linha[y])
-        print(self.mat)
 
     def stage(self):
         for x in range(self.lin):
             for y in range(self.col):
                 self.check_neigh(x, y)
-
+        print(self.mat)
+        self.mat = [x for x in self.new_mat]
 
     def check_neigh(self, x,y):
-    # TODO: Cuidar do new_mat zerando a cada iteração, apenas integrar ele no fim do fluxo, ou printar ele como resposta
+        # morta com 3 vivas vizinhas
         if self.mat[x][y] == 0:
             if self.check_alive(x, y) == 3:
                 self.new_mat[x][y] = 1
         else:
+            # viva com menos de 2 ou mais que 3
             if self.check_alive(x, y) < 2 or self.check_alive(x, y) > 3:
                 self.new_mat[x][y] = 0
+            # viva com 2 ou 3 vizinhos vivos
             elif self.check_alive(x, y) in [2,3]:
-                self.new_mat[x][y] = 0
+                self.new_mat[x][y] = 1
 
 
     def check_alive(self, x, y):
@@ -39,6 +41,8 @@ class JogoVida:
         poss = [(1, 1), (1, 0), (1, -1), (0, -1), (0, 1), (-1, -1), (-1, 0), (-1, 1)]
         for tup in poss:
             new_coord = tuple(map(sum, zip(coord, tup)))
+            if new_coord[0] < 0 or new_coord[1] < 0:
+                continue
             try:
                 if self.mat[new_coord[0]][new_coord[1]]:
                     count_alive += 1
@@ -56,3 +60,4 @@ if __name__ == '__main__':
     jogo = JogoVida(*[int(x) for x in input().split()])
     jogo.fill_mat()
     jogo.cycle(int(input()))
+
